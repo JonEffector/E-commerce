661f5438d1487ab8a184b5f1687ffe995bdf771b
JonEffector <senezachrisjonathan@gmail.com>
2021-07-11 19:25:39 +0200

    backend
    starting backend

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+export default {
+    products : [
+        {
+            _id:"1",
+            name:"Slim Shirt",
+            image:"/images/d1.jpg",
+            price:60,
+            brand:"Nike",
+            rating: 4.5,
+            numReviews:10
+        },
+        {
+            _id:"2",
+            name:"Fit Shirt",
+            image:"/images/d1.jpg",
+            price:45,
+            brand:"Lacostle",
+            rating: 4,
+            numReviews:5
+        },
+        {
+            _id:"3",
+            name:"Best Pants",
+            image:"/images/d1.jpg",
+            price:70,
+            brand:"Nike",
+            rating: 4.1,
+            numReviews:8
+        },
+        {
+            _id:"4",
+            name:"Slim Shirt",
+            image:"/images/d1.jpg",
+            price:60,
+            brand:"Nike",
+            rating: 4.5,
+            numReviews:10
+        },
+        {
+            _id:"5",
+            name:"Fit Shirt",
+            image:"/images/d1.jpg",
+            price:45,
+            brand:"Lacostle",
+            rating: 4,
+            numReviews:5
+        },
+        {
+            _id:"6",
+            name:"Best Pants",
+            image:"/images/d1.jpg",
+            price:70,
+            brand:"Nike",
+            rating: 4.1,
+            numReviews:8
+        }
+    ]
+}
