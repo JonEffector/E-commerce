62ab014630e0e073cd60aef46945d9f4952377e9
JonEffector <senezachrisjonathan@gmail.com>
2021-07-20 12:53:47 +0200

    Defining variables

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,3 +1,5 @@
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+
 function productListReducer(state = {products:[]},action){
 
     switch (action.type) {
